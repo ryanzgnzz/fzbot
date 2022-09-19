757d3d3bb5ff716d226b7758f8374954b2ade6ce
Ryanz Ofc <88297151+ryanzgnzz@users.noreply.github.com>
2022-09-19 13:13:37 +0700

    Add files via upload

--- plugins/creator.js
+++ plugins/creator.js
@@ -19,7 +19,7 @@ ${botdate}
     return conn.sendButton(m.chat, hiasan, sewa, img1, [["menu", usedPrefix + "menu"], ["profile", usedPrefix + "profile"]], m, {
         contextInfo: {
             externalAdReply: {
-                mediaUrl: "https://instagram.com/aguzfamilia",
+                mediaUrl: "https://instagram.com/ryanzstore",
                 mediaType: "VIDEO",
                 title: hiasan,
                 body: bottime,
