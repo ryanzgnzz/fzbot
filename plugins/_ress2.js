573e21f29ee62e052e11b8d6b8cfb516865ce45d
Ryanz Ofc <88297151+ryanzgnzz@users.noreply.github.com>
2022-09-19 18:53:30 +0700

    Update _ress2.js

--- plugins/_ress2.js
+++ plugins/_ress2.js
@@ -18,9 +18,9 @@ if (m.isBaileys) return
             await this.sendMessage(m.chat, { sticker : stc, thumbnail: await( await fetch(pp)).buffer() , contextInfo:{  externalAdReply: { showAdAttribution: true,
 mediaType:  1,
 mediaUrl: sig,
-title: 'FANGZ SIBUK ❗',
+title: 'YANZ SIBUK ❗',
 body: bottime,
-sourceUrl: 'https://chat.whatsapp.com/HpxyS6b45SoGIKG7LHddiM',
+sourceUrl: 'https://saweria.co/ryanzstore',
 thumbnail: await( await fetch(pp)).buffer()
   }
  }}, { quoted: m })
